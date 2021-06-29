5e23b8b3e6b662645274d7fd9c40354e04a3a836
Derrick Mehaffy <derrickmehaffy@gmail.com>
2021-06-29 12:08:51 -0700

    add find

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -3,6 +3,27 @@
 const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.article.search(ctx.query);
+    } else {
+      entities = await strapi.services.article.find(ctx.query);
+    }
+
+    let user = ctx.state.user;
+
+    if (!user || user.role.name !== "Administrator") {
+      entities.forEach((entity) => {
+        delete entity.users_permissions_user;
+      });
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.article })
+    );
+  },
+
   async findOne(ctx) {
     const { id } = ctx.params;
 
